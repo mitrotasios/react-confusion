3b9e14096526ced2232e9bfd5bbd2259388a126a
mitrotasios <a.mitrotasios13@gmail.com>
2021-02-28 14:54:42 +0200

    react animation components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,7 +5,8 @@ import {Link} from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form';
 import { addComment } from '../redux/ActionCreators';
 import { Loading } from './LoadingComponent';
-import { baseUrl } from '../shared/baseURL'
+import { baseUrl } from '../shared/baseURL';
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -102,13 +103,18 @@ class CommentForm extends Component {
 
 function RenderDish({dish}) {
     return (
-        <Card>
-            <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-            <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-            </CardBody>
-        </Card>
+        <FadeTransform in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50)'
+                }}>
+            <Card>
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </FadeTransform>
     )
 }
 
@@ -119,14 +125,19 @@ function RenderComments({comments, postComment, dishId}) {
             <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
                 <ul className="list-unstyled">
-                {comments.map((comment) => {
-                    let options = { year: "numeric", month: "short", day: "numeric" };
-                    return(                            
-                        <li key={comment.id} className="mb-2">
-                        <p>{comment.comment}</p>
-                        <p>-- {comment.author}, {new Date(comment.date).toLocaleDateString("en-US", options)}</p>                                
-                        </li>
-                    );})}                        
+                    <Stagger in>
+                        {comments.map((comment) => {
+                            let options = { year: "numeric", month: "short", day: "numeric" };
+                            return(     
+                                <Fade in>
+                                    <li key={comment.id} className="mb-2">
+                                    <p>{comment.comment}</p>
+                                    <p>-- {comment.author}, {new Date(comment.date).toLocaleDateString("en-US", options)}</p>                                
+                                    </li>
+                                </Fade>                       
+                            );
+                        })}       
+                    </Stagger>                 
                 </ul>
                 <CommentForm dishId={dishId} postComment={postComment}/>                          
             </div>
